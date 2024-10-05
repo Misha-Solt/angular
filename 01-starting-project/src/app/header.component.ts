260fe1842d32193df42d9f5c8a8079225e3e5488
Misha-Solt <mikhail.us@gmail.com>
2024-10-05 10:45:17 +0200

    Starting project added

---
+++ 01-starting-project/src/app/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+  //   styles: ['h1 {color: red}'],
+})
+export class HeaderComponent {}
